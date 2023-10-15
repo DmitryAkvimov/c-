76e83cc48407712870bc86097b3d66d7f240d1fb
DmitryAkvimov <dmitry.rvt.mst.ru@mail.ru>
2023-10-16 02:01:46 +0300

    Добавил task10

--- task10/Program.cs
+++ task10/Program.cs
@@ -4,9 +4,21 @@
 782 -> 8
 918 -> 1
 */
+
+while (true)
+{ 
 Console.Write("Введите трёхзначное число: ");
         int userNumber1 = Convert.ToInt32(Console.ReadLine());
-
+        int length = userNumber1.ToString().Length;  // Получаем количество цифр в числе
+       
+if (length == 3)
+{
         int result = userNumber1 /10 % 10;
-
         System.Console.WriteLine(result);
+        break;
+}
+else
+{
+    Console.WriteLine("Введено не трёхзначное число ");
+}
+}
