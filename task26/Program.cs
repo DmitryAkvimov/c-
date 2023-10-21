1e341b881942b76d0a6b1f300b5c3e286458e90b
DmitryAkvimov <dmitry.rvt.mst.ru@mail.ru>
2023-10-22 00:35:33 +0300

    Добавил task26

---
+++ task26/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+28. Напишите программу, которая принимает на вход 
+число N и выдаёт произвидение чисел от 1 до N.
+4-> 24
+5->120
+*/
+
+int Rrompt(string massage)
+{
+   System.Console.WriteLine(massage);
+   string value=Console.ReadLine();
+   int result=Convert.ToInt32(value);
+   return result;    
+}
+
+double MultNumber (int varA){
+
+    int result = 1;
+    for (int i = 1; i <= varA; i++)
+    {
+        result = result * i;
+    }
+
+    return result; 
+}
+
+int varA = Rrompt("Введите число: ");
+double result = MultNumber(varA);
+System.Console.WriteLine(result);
