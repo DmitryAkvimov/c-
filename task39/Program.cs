7b7882b49e31fc13d288cb520a520f6cfbb5602f
DmitryAkvimov <dmitry.rvt.mst.ru@mail.ru>
2023-11-06 04:48:00 +0300

    Добавил task39

---
+++ task39/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+39. Напишите программу, которая перевернёт одномерный массив 
+(последний элемент будет на первом месте, а первый - на последнем и т.д.)
+*/
+Console.Clear();
+int Rrompt(string massage)
+{
+   System.Console.WriteLine(massage);
+   string value=Console.ReadLine();
+   int result=Convert.ToInt32(value);
+   return result;    
+}
+
+void PrintArray(int[] arr)
+{
+    Console.ForgroundColor=ConsoleColor.Green;
+    System.Console.Write("[");
+    for (int i = 0; i < length; i++)
+    {
+        Console.ForgroundColor=ConsoleColor.Cyan;
+        Thread
+    }
+}
